03713493c0161df4788141c2e0fc4a08e2bcfda2
LitenApe <sonvo95@outlook.com>
2019-10-12 13:18:13 +0200

    feat: Add toCurrency
    Formats a number to a locale spesific representation of the number

---
+++ lib/toCurrency.ts
@@ -0,0 +1,19 @@
+/**
+ * Converts an number to a value format which
+ * fits the supplied locale.
+ * @param {string} value
+ * @returns {string} local spesific number format
+ * 
+ * @example
+ * const number = 123456.789;
+ * toCurrency()(number); // => "123 456.789"
+ * toCurrency("de-DE")(number) // => "123.456,789"
+ * toCurrency("en-IN")(number) // => "1,23,456.789"
+ */
+function toCurrency(locale: string = "nb-NO"): (value: number) => string {
+  return function(value: number): string {
+    return new Intl.NumberFormat(locale).format(value);
+  };
+}
+
+export default toCurrency;
