afd64c8404c3761ab51dd747527ae5ae25397c25
LitenApe <sonvo95@outlook.com>
2019-10-12 17:52:47 +0200

    chore: Add docstring to toNumberFormat

--- lib/toNumberFormat.ts
+++ lib/toNumberFormat.ts
@@ -1,3 +1,14 @@
+/**
+ * Converts the value sent to match the supplied format
+ * @param {string} format mask e.g. '### ## ###'
+ * @returns {string} masked value
+ * 
+ * @example
+ * toNumberFormat("### ## ###")("123456789"); // => "123 45 678"
+ * toNumberFormat("##/##/####")("123456789"); // => "12/34/5678"
+ * toNumberFormat("###### #####")("123456789123") // => "123456 78912"
+ * toNumberFormat("### ### ###")("12345"); // => "123 45"
+ */
 export function toNumberFormat(format: string): (value: string) => string {
   return function converter(value: string): string {
     if (!format) { return value; }
