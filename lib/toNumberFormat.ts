1abb1bd3c625443bedbd4427cd2a0a0ff8ba613b
LitenApe <sonvo95@outlook.com>
2019-10-12 18:15:34 +0200

    refactor: Improve toNumberFormat
    Rewrote the function to be more compact and readable

--- lib/toNumberFormat.ts
+++ lib/toNumberFormat.ts
@@ -14,13 +14,11 @@ export function toNumberFormat(format: string): (value: string) => string {
     if (!format) { return value; }
 
     let processed = value.replace(/\D/g, "");
-
-    for (let i = 0; i < format.length && i < processed.length; i++) {
-      if (format.charAt(i) !== "#") {
-        processed = processed.slice(0, i) + format.charAt(i) + processed.slice(i);
-      }
-    }
-
-    return processed.slice(0, format.length);
+    return format.replace(/#/g, () => {
+      if (!processed) return "";
+      const char = processed.charAt(0);
+      processed = processed.substr(1);
+      return char;
+    }).trim();
   };
 }
