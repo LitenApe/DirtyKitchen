7b62093b683e41b64560b83731ac7cfeaa677121
LitenApe <sonvo95@outlook.com>
2019-10-12 12:16:14 +0200

    feat: Add uniqueID generator
    Generate unique IDs with an optional or supplied prefix.

---
+++ lib/uniqueId.ts
@@ -0,0 +1,26 @@
+/**
+ * Used to keep an history of used id's to
+ * avoid conflicts and always start on "1"
+ * with new prefixes.
+ */
+const dirtyIdHistory: {[key: string]: number} = {};
+
+/**
+ * Generates a unique ID and prefixes the supplied
+ * string to it.
+ * @param {string} prefix Prefix for the ID. Defaults to "dirty_"
+ * @returns {string} Returns the unique ID.
+ * 
+ * @example
+ * dirtyIds(); // => "dirty_1"
+ * dirtyIds("user_"); // "user_1"
+ */
+function uniqueId(prefix: string = "dirty_"): string {
+  if (!dirtyIdHistory[prefix]) {
+    dirtyIdHistory[prefix] = 0;
+  }
+
+  return `${prefix}${++dirtyIdHistory[prefix]}`;
+}
+
+export default uniqueId;
