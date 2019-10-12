0e649b9cfdbab3915d5989a4deb66f6ac9d95ea7
LitenApe <sonvo95@outlook.com>
2019-10-12 13:03:35 +0200

    feat: Add toNumber
    Strip all non-arabic-numeral characters except '.'

---
+++ lib/toNumber.ts
@@ -0,0 +1,15 @@
+/**
+ * Strips all all non-arabic-numerals and
+ * symbols except '.';
+ * @param {string} value value to be stripped of non-arabic-numerals
+ * @returns {string} processed value
+ * 
+ * @example
+ * toNumber("1a2b 3c"); // => "123"
+ * toNumber("12,3a.123"); // => "123.123"
+ */
+function toNumber(value: string = ""): string {
+  return value.replace(/[^0-9.]/g, "")
+}
+
+export default toNumber;
