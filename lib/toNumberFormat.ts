16e7ebca772b80c99e5beee7e8b51e945fa65ad4
LitenApe <sonvo95@outlook.com>
2019-10-13 22:13:21 +0200

    fix Dangling symbols in number format
    The previous implementation resulted in dangling symbols for arguments which were shorter than the format in length. An underscore is now insterted as an placeholder for remaining characters to provide the user with more information regarding expected input.

--- lib/toNumberFormat.ts
+++ lib/toNumberFormat.ts
@@ -7,18 +7,17 @@
  * toNumberFormat("### ## ###")("123456789"); // => "123 45 678"
  * toNumberFormat("##/##/####")("123456789"); // => "12/34/5678"
  * toNumberFormat("###### #####")("123456789123") // => "123456 78912"
- * toNumberFormat("### ### ###")("12345"); // => "123 45"
+ * toNumberFormat("### ### ###")("12345"); // => "123 45_ ___"
  */
 export function toNumberFormat(format: string): (value: string) => string {
   return function converter(value: string): string {
     if (!format) { return value; }
 
-    let processed = value.replace(/\D/g, "");
-    return format.replace(/#/g, () => {
-      if (!processed) return "";
-      const char = processed.charAt(0);
-      processed = processed.substr(1);
-      return char;
-    }).trim();
+    const digits = value.match(/\d/g);
+
+    return format.replace(/#/g, (): string => {
+      if (!digits || digits.length === 0) return "_";
+      return digits.shift() as string;
+    });
   };
 }
