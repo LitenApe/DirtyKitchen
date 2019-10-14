1701f0a5dd1beb492db7315a642dc627938b2deb
LitenApe <sonvo95@outlook.com>
2019-10-14 21:18:45 +0200

    fix: toNumberFormat appended symbols stops input
    Appending symbols to the end of the input value makes it impossible for the user to change the input. Changed over to use a for loop which stops when either format or value has reached their end.

--- lib/toNumberFormat.ts
+++ lib/toNumberFormat.ts
@@ -2,22 +2,27 @@
  * Converts the value sent to match the supplied format
  * @param {string} format mask e.g. '### ## ###'
  * @returns {string} masked value
- * 
+ *
  * @example
  * toNumberFormat("### ## ###")("123456789"); // => "123 45 678"
  * toNumberFormat("##/##/####")("123456789"); // => "12/34/5678"
  * toNumberFormat("###### #####")("123456789123") // => "123456 78912"
- * toNumberFormat("### ### ###")("12345"); // => "123 45_ ___"
+ * toNumberFormat("### ### ###")("12345"); // => "123 45"
  */
 export function toNumberFormat(format: string): (value: string) => string {
   return function converter(value: string): string {
     if (!format) { return value; }
 
     const digits = value.match(/\d/g);
+    let result = "";
 
-    return format.replace(/#/g, (): string => {
-      if (!digits || digits.length === 0) return "_";
-      return digits.shift() as string;
-    });
+    if (!digits) return result;
+
+    for (let i = 0; i < format.length && digits.length > 0; i++) {
+      const char = format.charAt(i);
+      result += char === "#" && digits ? digits.shift() : char;
+    }
+
+    return result;
   };
 }
