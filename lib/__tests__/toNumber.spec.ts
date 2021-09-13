26dd3f4c388a31fc6c3b8a1e0d9e69acbd9d24ee
Son Thanh Vo <sonvo95@outlook.com>
2021-09-13 18:52:00 +0200

    tests: add jest for writing tests

---
+++ lib/__tests__/toNumber.spec.ts
@@ -0,0 +1,9 @@
+import { toNumber } from '../toNumber';
+
+test('remove all non numeric characters', () => {
+  expect(toNumber('1a2b 3c!@#%#@%$^&*()')).toBe('123');
+});
+
+test('preserves "." as the only valid separator', () => {
+  expect(toNumber('12,3.123')).toBe('123.123');
+});
