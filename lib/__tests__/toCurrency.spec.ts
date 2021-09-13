34c246c5fc99be9fc36efd1247b886e91349afc2
Son Thanh Vo <sonvo95@outlook.com>
2021-09-13 19:48:43 +0200

    test: add tests for toCurrency

---
+++ lib/__tests__/toCurrency.spec.ts
@@ -0,0 +1,54 @@
+import { toCurrency } from '../toCurrency';
+
+const testValues = [0.2, 123.2, 1234.2, 1234567.2];
+
+function appendResult(locale: string) {
+  return testValues.map((value) => [
+    value,
+    Intl.NumberFormat(locale).format(value),
+  ]);
+}
+
+describe('converts to norwegian format by default', () => {
+  test.each(appendResult('nb-NO'))(
+    'converts %value to %result',
+    (value, result) => {
+      expect(toCurrency()(value as number)).toBe(result);
+    }
+  );
+});
+
+describe('converts to other formats format when specified', () => {
+  test.each(appendResult('en-IN'))(
+    '[en-IN] converts %value to %result',
+    (value, result) => {
+      expect(toCurrency('en-IN')(value as number)).toBe(result);
+    }
+  );
+
+  test.each(appendResult('ja-JP'))(
+    '[ja-JP] converts %value to %result',
+    (value, result) => {
+      expect(toCurrency('ja-JP')(value as number)).toBe(result);
+    }
+  );
+
+  test.each(appendResult('de-DE'))(
+    '[de-DE] converts %value to %result',
+    (value, result) => {
+      expect(toCurrency('de-DE')(value as number)).toBe(result);
+    }
+  );
+
+  test('confirms that the various locales differ', () => {
+    const number = 123456.789;
+    const no = toCurrency('nb-NO')(number);
+    const en = toCurrency('en-IN')(number);
+    const ja = toCurrency('ja-JP')(number);
+    const de = toCurrency('de-DE')(number);
+
+    expect(no).not.toBe(en);
+    expect(en).not.toBe(ja);
+    expect(ja).not.toBe(de);
+  });
+});
