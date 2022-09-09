d34a8b079a3e465507c78d448997f80a997aeeb7
Son Thanh Vo <sonvo95@outlook.com>
2022-09-09 16:38:43 +0200

    chore: add eslint/import

--- lib/uniqueId/uniqueId.ts
+++ lib/uniqueId/uniqueId.ts
@@ -3,14 +3,14 @@
  * avoid conflicts and always start on "1"
  * with new prefixes.
  */
-const dirtyIdHistory: {[key: string]: number} = {};
+const dirtyIdHistory: { [key: string]: number } = {};
 
 /**
  * Generates a unique ID and prefixes the supplied
  * string to it.
  * @param {string} prefix Prefix for the ID. Defaults to "dirty_"
  * @returns {string} Returns the unique ID.
- * 
+ *
  * @example
  * dirtyIds(); // => "dirty_1"
  * dirtyIds("user_"); // "user_1"
