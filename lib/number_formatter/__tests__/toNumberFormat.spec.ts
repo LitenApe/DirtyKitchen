ac36804ec9d5c2767975c2728d5b93f4808891d6
Son Thanh Vo <sonvo95@outlook.com>
2022-09-09 15:51:15 +0200

    test: use test keyword instead of it

--- lib/number_formatter/__tests__/toNumberFormat.spec.ts
+++ lib/number_formatter/__tests__/toNumberFormat.spec.ts
@@ -1,14 +1,14 @@
 import { toNumberFormat } from '../toNumberFormat';
 
 describe('formats number string according to pre-determined format', () => {
-  it('non numbers are removed', () => {
+  test('non numbers are removed', () => {
     const converter = toNumberFormat('### ## ###');
 
     expect(converter('adfjkalfa')).toBe('');
     expect(converter('a1b2b3c4d5')).toBe('123 45');
   });
 
-  it('strings shorter then format is handled correctly', () => {
+  test('strings shorter then format is handled correctly', () => {
     const converter = toNumberFormat('### ## ###');
 
     expect(converter('')).toBe('');
@@ -18,7 +18,7 @@ describe('formats number string according to pre-determined format', () => {
     expect(converter('123456')).toBe('123 45 6');
   });
 
-  it('string of propper length is handled correctly', () => {
+  test('string of propper length is handled correctly', () => {
     const converter = toNumberFormat('### ## ###');
 
     expect(converter('12345678')).toBe('123 45 678');
