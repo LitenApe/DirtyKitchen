3c365f5c924201c1c3ba62f00159989a3491672a
Son Thanh Vo <sonvo95@outlook.com>
2022-09-09 15:50:25 +0200

    test: wrap tests in a describe

--- lib/number_formatter/__tests__/toNumber.spec.ts
+++ lib/number_formatter/__tests__/toNumber.spec.ts
@@ -1,9 +1,11 @@
 import { toNumber } from '../toNumber';
 
-test('remove all non numeric characters', () => {
-  expect(toNumber('1a2b 3c!@#%#@%$^&*()')).toBe('123');
-});
+describe('formats and process string to only contain valid number or symbols associated with numbers', () => {
+  test('remove all non numeric characters', () => {
+    expect(toNumber('1a2b 3c!@#%#@%$^&*()')).toBe('123');
+  });
 
-test('preserves "." as the only valid separator', () => {
-  expect(toNumber('12,3.123')).toBe('123.123');
+  test('preserves "." as the only valid separator', () => {
+    expect(toNumber('12,3.123')).toBe('123.123');
+  });
 });
