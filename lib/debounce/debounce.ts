ab29381606f3bd6a0a1fee53a754a7d8018dd7ab
Son Thanh Vo <sonvo95@outlook.com>
2022-09-12 15:51:49 +0200

    feat: add debounce function

---
+++ lib/debounce/debounce.ts
@@ -0,0 +1,20 @@
+import { isUndefined } from '../type_checks';
+
+type GenericFunction<P extends Array<unknown>> = (...params: P) => void;
+
+export function debounce<P extends Array<unknown>>(
+    func: GenericFunction<P>,
+    delay = 300,
+) {
+    let timer: NodeJS.Timeout;
+
+    return (...params: P) => {
+        if (!isUndefined(timer)) {
+            clearTimeout(timer);
+        }
+
+        timer = setTimeout(() => {
+            func(...params);
+        }, delay);
+    };
+}
