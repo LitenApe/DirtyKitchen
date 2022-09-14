c8556bf6d59a1147226a2985fd3eff162fa43462
Son Thanh Vo <sonvo95@outlook.com>
2022-09-14 15:03:59 +0200

    refactor: move cancel and flush out

--- lib/debounce/debounce.ts
+++ lib/debounce/debounce.ts
@@ -1,35 +1,41 @@
+import { isDefined } from '../type_checks';
+
 /**
  * Delay function invocation until after a set time
  * has elapsed since the last time the debounced function
  * was last invoked.
  * @param func function we want to debounce
  * @param delay time in milliseconds before the function is invoked
- * @returns debounced function
+ * @returns debounced function and utility functions
  */
 export function debounce<P extends Array<unknown>>(
   func: (...params: P) => void,
   delay = 300,
-): (...params: P) => void {
+): [(...params: P) => void, { flush: () => void; cancel: () => void }] {
   let timer: NodeJS.Timeout | undefined;
+  let lastParams: P | undefined;
 
-  return function (...params: P): { flush: () => void; cancel: () => void } {
+  function debouncedFunction(...params: P): void {
+    lastParams = params;
     clearTimeout(timer);
 
     timer = setTimeout(() => {
       func(...params);
       timer = undefined;
     }, delay);
+  }
 
-    function cancel() {
-      clearTimeout(timer);
-      timer = undefined;
+  function cancel() {
+    clearTimeout(timer);
+    timer = undefined;
+  }
+
+  function flush(): void {
+    cancel();
+    if (isDefined(lastParams)) {
+      func(...lastParams);
     }
+  }
 
-    function flush(): void {
-      cancel();
-      func(...params);
-    }
-
-    return { flush, cancel };
-  };
+  return [debouncedFunction, { flush, cancel }];
 }
