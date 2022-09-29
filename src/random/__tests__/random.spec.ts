74ecfd1e286362866d365a32dd57edac24165ff2
Son Thanh Vo <sonvo95@outlook.com>
2022-09-29 18:35:23 +0200

    test: improve random tests

--- src/random/__tests__/random.spec.ts
+++ src/random/__tests__/random.spec.ts
@@ -21,6 +21,16 @@ describe('random', () => {
     expect(random(max / 10, max)).toBe(max);
   });
 
+  test('returned number is middle of boundaries when random number is 0.5', () => {
+    const mock = jest.spyOn(Math, 'random');
+    mock.mockReturnValue(0.5);
+
+    const max = 10000;
+    const min = max / 10;
+    const expected = 0.5 * (max - min) + min;
+    expect(random(min, max)).toBe(expected);
+  });
+
   test('returned number is float by default', () => {
     const value = random(10, 100);
     expect(value).not.toEqual(parseInt(String(value)));
@@ -38,13 +48,43 @@ describe('retrieves random element from array', () => {
     expect(value).toBeUndefined();
   });
 
-  test('returns random element in array', () => {
-    const iterations = 1000;
+  test('returns first element in array when random number is 0', () => {
+    const mock = jest.spyOn(Math, 'random');
+    mock.mockReturnValue(0);
 
-    const data = Array.from({ length: 100 }, () => Math.random());
-    Array.from({ length: iterations }).forEach(() => {
-      const value = random(data);
-      expect(data.includes(value)).toBe(true);
-    });
+    const array = ['first', 'second', 'third', 'fourth'];
+
+    const value = random(array);
+    expect(value).toBe('first');
+  });
+
+  test('returns last element in array when random number is 1', () => {
+    const mock = jest.spyOn(Math, 'random');
+    mock.mockReturnValue(1);
+
+    const array = ['first', 'second', 'third', 'fourth'];
+
+    const value = random(array);
+    expect(value).toBe('fourth');
+  });
+
+  test('returns second element in array when random number is 0.5', () => {
+    const mock = jest.spyOn(Math, 'random');
+    mock.mockReturnValue(0.5);
+
+    const array = ['first', 'second', 'third', 'fourth'];
+
+    const value = random(array);
+    expect(value).toBe('second');
+  });
+
+  test('returns third element in array when random number is 0.75', () => {
+    const mock = jest.spyOn(Math, 'random');
+    mock.mockReturnValue(0.75);
+
+    const array = ['first', 'second', 'third', 'fourth'];
+
+    const value = random(array);
+    expect(value).toBe('third');
   });
 });
